54531dbacc8a4de9d13cb64964c0e871f86b3b61
Fabianne Costa <fabiannecs@hotmail.com>
2019-12-18 10:14:03 -0300

    :)

--- crud-vue/src/main.js
+++ crud-vue/src/main.js
@@ -32,14 +32,14 @@ Vue.component('b-button-close', BButtonClose)
 
 
 var config = {
-  apiKey: "AIzaSyAz98EuYpMN7vX6CgOh6YNDBbsEKIwjWjk",
-  authDomain: "crud-vue-4e470.firebaseapp.com",
-  databaseURL: "https://crud-vue-4e470.firebaseio.com",
-  projectId: "crud-vue-4e470",
-  storageBucket: "crud-vue-4e470.appspot.com",
-  messagingSenderId: "346353484914",
-  appId: "1:346353484914:web:8b5c30c65b14097b386eeb",
-  measurementId: "G-066T42NZ4M"
+  apiKey: "********",
+  authDomain: "********.firebaseapp.com",
+  databaseURL: "https://********.firebaseio.com",
+  projectId: "********",
+  storageBucket: "********.appspot.com",
+  messagingSenderId: "********",
+  appId: "********",
+  measurementId: "********"
 };
 
 firebase.initializeApp(config);
